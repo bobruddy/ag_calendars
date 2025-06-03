b28c0d2682f73e16b8089322c860a6b62bc82e2d
Robert Ruddy <bob@ruddy.net>
2025-06-03 10:49:28 -0400

    fixed some pathing issues

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd ${SCRIPT_DIR}
+
 . .venv/bin/activate
 python3 get_cal.py
 . .env
 cp index.html ${AG_SAVE_DIR}
 
 rsync -av ./${AG_SAVE_DIR}/ ${AG_DEST_SERVER}:${AG_DEST_BASE_DIR}${AG_SAVE_DIR}/
+
+cd -
