956f323ac12b5bfb84b47a507ee57754308163eb
Robert Ruddy <bob@ruddy.net>
2025-06-19 08:38:15 -0400

    added last updated date to index.html

--- run.sh
+++ run.sh
@@ -9,6 +9,10 @@ python3 get_cal.py
 . .env
 cp index.html ${AG_SAVE_DIR}
 
+oldest_file=$(ls -1t ${AG_SAVE_DIR}/*.ics | tail -n 1) 
+formatted_date=$(date -r "${oldest_file}" +"%Y-%m-%d %H:%M")
+sed "s/update_date/${formatted_date}/" index.html > ${AG_SAVE_DIR}/index.html
+
 rsync -av ./${AG_SAVE_DIR}/ ${AG_DEST_SERVER}:${AG_DEST_BASE_DIR}${AG_SAVE_DIR}/
 
 cd -
